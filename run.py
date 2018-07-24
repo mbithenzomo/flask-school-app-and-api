2de0ac24c00f2e1a2a2883f93c06a63ec2ab49ac
Mbithe Nzomo <mbithe.nzomo@andela.com>
2018-07-24 17:17:49 +0300

    [Feature #159261513] Add endpoints for CRUD operations on teachers

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from app import api, app
 from app.resources import Index
 from app.resources.auth import UserLogin, UserRegister
 from app.resources.students import StudentListAPI, StudentAPI
+from app.resources.teachers import TeacherListAPI, TeacherAPI
 
 
 """ Defining the API endpoints """
@@ -10,6 +11,8 @@ api.add_resource(UserRegister, "/auth/register")
 api.add_resource(UserLogin, "/auth/login")
 api.add_resource(StudentListAPI, "/students")
 api.add_resource(StudentAPI, "/students/<string:id>")
+api.add_resource(TeacherListAPI, "/teachers")
+api.add_resource(TeacherAPI, "/teachers/<string:id>")
 
 if __name__ == '__main__':
     app.run()
