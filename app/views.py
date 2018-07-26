0f8d19f860d15ba384823e9d1f70fd73cf38e90d
Mbithe Nzomo <mbithe.nzomo@andela.com>
2018-07-26 09:02:55 +0300

    [Chore #159268104] Add view to print all URLs to the console

--- app/views.py
+++ app/views.py
@@ -36,6 +36,26 @@ def get_token():
     return {"Authorization": token}
 
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    print links
+    return render_template('login.html', title='Login')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
